345cdd72d4e62bbd7836c5f183b542dffc662144
MD, MASHRUR RAHMAN <md.mashrurrahman484@gmail.com>
2025-12-10 17:49:53 +0600

    Set matplotlib to use 'Agg' backend

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
